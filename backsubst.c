768087e126504ff8053be2876e4212ebd68544cb
Antoni Zajac <zajac.antek@o2.pl>
2021-12-16 22:43:24 +0100

    backsubst.c updated

--- backsubst.c
+++ backsubst.c
@@ -8,27 +8,31 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 	int r = mat->r;
 	int c = mat->c;
 	
-	int i, j, k;
-	double sum;
-        double v, g;
+	int i, j;
 
 	// sprawdz rozmiar macierzy b i mat
 	if (b->c > 1 || b->r != r || r != c) {
 		return 2;
 	}
 
-	j = r - 1;
-	while (j >= 0) {
-		i = c - 1;
-		sum = 0;
-		while (i >= j) {
-			sum += b->data[j][0] / mat->data[j][i];
-			x->data[j];
-		}
+	j = r;
+	while (--j >= 0) {
+		i = c;
 		
+		double sum = 0;
+		while (--i > j) {
+			// dla macierzy x indeks kolumny jest indeksem aktywnego wiersza
+			sum += x->data[i][0] * mat->data[j][i];
+		}
+
+		// sprawdz czy na diagonali nie ma 0
+		if (mat->data[j][i] == 0) {
+			return 1;
+		}
+
+		x->data[j][0] = ( b->data[j][0] - sum ) / mat->data[j][i];		
 	}
 
-	
 	return 0;
 }
 
